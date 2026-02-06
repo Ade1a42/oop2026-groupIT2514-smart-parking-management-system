3f672b0a588850fc18068f0c69ca68c15e60fe86
anurzhankyz <addel4279@gmail.com>
2026-02-07 00:16:16 +0500

    I used builder method for Reservation and added new exception so before creating builder it checks

--- src/main/parking/model/Reservation.java
+++ src/main/parking/model/Reservation.java
@@ -11,13 +11,73 @@ public class Reservation {
     private double totalCost;
     private String status;  // active, completed, canceled
 
+    private Reservation(ReservationBuilder builder) {
+        this.vehicleId = builder.vehicleId;
+        this.spotId = builder.spotId;
+        this.startTime = builder.startTime;
+        this.endTime = builder.endTime;
+        this.totalCost = builder.totalCost;
+        this.status = builder.status;
+    }
 
+    // existing constructors for backward compatibility
     public Reservation() {}
     public Reservation(int vehicleId, int spotId) {
         this.vehicleId = vehicleId;
         this.spotId = spotId;
         this.startTime = LocalDateTime.now();
         this.status = "ACTIVE";
+    }
+
+    public static class ReservationBuilder {
+        private final int vehicleId;
+        private final int spotId;
+
+        private LocalDateTime startTime = LocalDateTime.now();
+        private LocalDateTime endTime;
+        private double totalCost = 0.0;
+        private String status = "ACTIVE";
+
+        public ReservationBuilder(int vehicleId, int spotId) {
+            this.vehicleId = vehicleId;
+            this.spotId = spotId;
+        }
+
+        public ReservationBuilder withStartTime(LocalDateTime startTime) {
+            this.startTime = startTime;
+            return this;
+        }
+
+        public ReservationBuilder withEndTime(LocalDateTime endTime) {
+            this.endTime = endTime;
+            return this;
+        }
+
+        public ReservationBuilder withTotalCost(double totalCost) {
+            this.totalCost = totalCost;
+            return this;
+        }
+
+        public ReservationBuilder withStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Reservation build() {
+            validateReservation();
+
+            return new Reservation(this);
+        }
+
+
+        private void validateReservation() {
+            if (endTime != null && endTime.isBefore(startTime)) {
+                throw new IllegalArgumentException("End time cannot be before start time!");
+            }
+            if (totalCost < 0) {
+                throw new IllegalArgumentException("Total cost cannot be negative!");
+            }
+        }
     }
 
     // setters and getters
